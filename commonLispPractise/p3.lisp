5ffa4ce23863c80d8238fc76c2f06450e09ef2b1
chenYe <cyanskywing@outlook.com>
2016-03-14 12:16:14 +0800

    simple

---
+++ commonLispPractise/p3.lisp
@@ -0,0 +1,23 @@
+(defvar *db* nil)
+
+(defun makeCd (title artist rating ripped)
+  (list :title title :artist artist :rating rating :ripped ripped))
+
+(defun add (cd)
+  (push cd *db*))
+
+(defun dump-db()
+  (dolist (cd *db*)
+          (format t "~{~a: ~10t~a~%~}~%" cd)))
+
+(defun promptRead (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-for-cd()
+  (makeCd
+   (promptRead "Title")
+   (promptRead "Artist")
+   (promptRead "Rating")
+   (promptRead "Ripped [y/n]")))
